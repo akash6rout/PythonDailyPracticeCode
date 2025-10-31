64d4e53ad7fedfc245e7dbd6ea9e07fa560c8a61
Akash Kumar Rout <akash06.rout@gmail.com>
2025-10-31 22:32:48 +0530

    formating

---
+++ Formating.py
@@ -0,0 +1,19 @@
+
+# #formatting f-string
+# model = 'TVS Ntorq'
+# price = 10500
+
+# print(f"The {model} costs {price}.")
+
+# model = "TVS"
+# brand = model[::-1]
+# print(brand)
+
+# format = '%.3f %s is $%d'
+
+# format %(6.4560,'python prasad',1)
+
+# print("My name is %s and weight is %d kgs!" %('xyz',20.5))
+
+name = input("Enter Your Name : ")
+print(f"Hello,{name}!")
