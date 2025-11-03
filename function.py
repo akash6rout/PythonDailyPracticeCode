081fc622c33c00ca474f0bfdc548acece4334757
Akash Kumar Rout <akash06.rout@gmail.com>
2025-11-03 22:42:53 +0530

    string function

---
+++ function.py
@@ -0,0 +1,26 @@
+# # s = "HELLO"
+
+# # result = s.center(20,'*')
+
+# # print(result)
+
+# #crating the string
+# string = 'akash Kumar Rout'
+# #finding the substring
+# substring = 'a'
+# #count the repeated string
+# count = string.count(substring)
+# #print the count
+# print("The count is :",count)
+
+# #print using f string
+# print(f"the count is {count}")
+
+# count = string.count(substring,7,19)
+
+string = 'this is a string example!!! Is this really is a string'
+
+result = string.replace("is","was",2)
+print(result)
+
+# print(":".join(string))
